da13f6d7ae853273378ddd43d3c01743effe0996
sahil-1234 <sahil.srivastava0170@gmail.com>
2022-01-16 16:55:27 +0530

    logger

---
+++ idle_page.py
@@ -0,0 +1,13 @@
+from logger import listen_to_keyboard
+from tkinter import *
+window=Tk()
+window.geometry("400x400")
+label=Label(window, text="run keylogger")
+label.pack()
+
+#listen_to_keyboard() 
+
+
+window.mainloop()
+
+
