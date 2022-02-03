5a9cff268f983158d6703e504d05c8b7353c3e7b
Dipanshu Singh <58115782+dipanshuhappy@users.noreply.github.com>
2022-02-03 18:15:45 +0530

    remove comments

--- login_page.py
+++ login_page.py
@@ -1,29 +1,4 @@
 
-
-"""import tkinter
-import login
-window=tkinter.Tk()
-window.geometry("800x200")
-top_frame=tkinter.Frame(window).pack()
-tkinter.Label(window,top_frame, text="Username").pack()
-user_entry=tkinter.Entry(window)
-user_entry.pack()
-tkinter.Label(window, text="Password").pack()
-pass_entry=tkinter.Entry(window)
-pass_entry.pack()
-def clicked():
-    user_name=user_entry.get()
-    password=pass_entry.get()
-    if login.does_user_exist(user_name): 
-         
-        if login.is_password_valid(user_name, password):
-            pass
-        else:
-            tkinter.Label(window,text="Passworrd invalid").pack()
-    else:
-        tkinter.Label(window,text="USer does not exist").pack()
-tkinter.Button(window, text="Enter",command=clicked).pack()      
-window.mainloop()"""
 import tkinter as tk
 import login
 from tkinter import ttk
